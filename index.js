62354993bfcae34541d88d45b5bc786e3030d046
Cagatay Wilhelm <cagatayttnc@web.de>
2025-02-03 16:15:27 +0100

    update

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 import { question } from "readline-sync";
 
-const riddle = question("Wo findest du Wälder ohne Bäume, Flüsse ohne Wasser und Städte ohne Häuser?");
+const riddle = "Wo findest du Wälder ohne Bäume, Flüsse ohne Wasser und Städte ohne Häuser? ";
 const solution = "landkarte";
 
 let versuche = 3;
 
 while(versuche > 0) {
-    let answer = readline.question(riddle + " ");
+    let answer = question(riddle + " ");
 
-    if(answer.toLowercase() === solution){
+    if(answer.toLowerCase() === solution){
         console.log("Du hast das Rätsels Lösung!");
         break;
     } else {
         versuche--;
         if( versuche > 0) {
-            console.log();
+            console.log(`Nein! Veruche es noch einmal. Du hast noch ${versuche}`);
+        } else {
+            console.log("Leider falsch! HA HA HA! Du wirst es niemals lösen können!");
         }
     }
 }
