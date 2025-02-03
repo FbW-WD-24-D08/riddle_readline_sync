926542bfa3ac4065706e9dfc505040d5612220be
Cagatay Wilhelm <cagatayttnc@web.de>
2025-02-03 15:41:02 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { question } from "readline-sync";
 
 const riddle = question("Wo findest du Wälder ohne Bäume, Flüsse ohne Wasser und Städte ohne Häuser?");
-const answer = "landkarte";
+const solution = "landkarte";
 
 let versuche = 3;
 
 while(versuche > 0) {
-    let answer = readline.question(riddle + " ");
+    let solution = readline.question(riddle + " ");
 }
+
+
